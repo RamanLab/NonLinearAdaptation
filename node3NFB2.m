7d4be074bccaca7a4c40ed9baea8527a67761fca
Priyan <37521742+priyan295@users.noreply.github.com>
2024-05-28 20:40:26 -0400

    Update node3NFB2.m

--- node3NFB2.m
+++ node3NFB2.m
@@ -9,7 +9,7 @@ Fa=6;
 x_star=[0.4;0.64;0.5];
  
 dydt_1=Kia*u*(1-y(1))-10*y(1)/(y(1)+1);
-dydt_2=Kbc*y(3)-Fb*y(2)/(KMB+y(2));
+dydt_2=Kbc*y(3)-Fb*y(2)/(KMB+y(2)); % Violation: Negative feedback without buffer
 dydt_3=Kca*y(1)*(1-y(3))-Kcb*y(2)*y(3)/(y(3)+1);
 dydt=[dydt_1;dydt_2;dydt_3];
 end
