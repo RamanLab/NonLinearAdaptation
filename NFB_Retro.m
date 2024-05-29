1e18337d1c88edff6eb4d767a6299ba7db2b9a0a
Priyan <37521742+priyan295@users.noreply.github.com>
2024-05-28 20:35:37 -0400

    Update NFB_Retro.m

--- NFB_Retro.m
+++ NFB_Retro.m
@@ -1,3 +1,4 @@
+%% Run MUL_NFB_NON_LIN before. This code generates the figure 7 of the manuscript. We assess the retroactivity property of the NFB module
 subplot(1,2,1)
 plot(out.tout,out.Input)
 subplot(1,2,2)
