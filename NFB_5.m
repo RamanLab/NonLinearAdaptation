7f33cebfcaff246109d4dada703b32532d21f499
Priyan <37521742+priyan295@users.noreply.github.com>
2024-05-28 20:33:08 -0400

    Update NFB_5.m

--- NFB_5.m
+++ NFB_5.m
@@ -1,3 +1,4 @@
+%% Simulation study for sufficient condition for Global adaptation with negative feedback with buffer (Fig 6 of the manuscript). Run MUL_NFB_NON_LIN SIMULINK model file before.
 y0=100*lhsdesign(5,100);
 figure
 tspan=[0 100];
@@ -21,8 +22,8 @@ for k=1:1:100
 [t1,y1]=ode45(@(t,y)node5NFB(t,y,200),tspan,y(length(y),:)');
 [t2,y2]=ode45(@(t,y)node5NFB(t,y,250),tspan,y1(length(y1),:)');
 %subplot(1,3,3)
-%plot([y(:,1);y1(:,1);y2(:,1)],[y(:,2);y1(:,2);y2(:,2)])
-t=[t;t(end)+t1;t1(end)+t(end)+t2];
-plot(t,log2(1+[y(:,2);y1(:,2);y2(:,2)]))
+plot([y(:,1);y1(:,1);y2(:,1)],[y(:,2);y1(:,2);y2(:,2)])
+%t=[t;t(end)+t1;t1(end)+t(end)+t2];
+%plot(t,log2(1+[y(:,2);y1(:,2);y2(:,2)]))
 hold on
 end
